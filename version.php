5914d559060c335355060f7a567944f873354220
valery <valery.fremaux@gmail.com>
2016-10-30 14:48:39 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -15,22 +15,22 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details.
+ * Version detals.
  *
- * @package    report_patches
- * @category   report
- * @author     Valery Fremaux (valery.fremaux@gmail.com)
- * @copyright  2010 onwards Valery Fremaux (valery.fremaux@gmail.com)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package     report_patches
+ * @category    report
+ * @author      Valery Fremaux <valery.fremaux@gmail.com>
+ * @copyright   2010 Valery Fremaux (valery.fremaux@gmail.com)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012062800;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2016051900;        // Requires this Moodle version
-$plugin->component = 'report_patches'; // Full name of the plugin (used for diagnostics)
-$plugin->release = '3.1.0 (Build 2012062800)'; // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2012062800;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2014050800;        // Requires this Moodle version.
+$plugin->component = 'report_patches'; // Full name of the plugin (used for diagnostics).
+$plugin->release = '2.7.0 (Build 2012062800)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '2.7.0000';
