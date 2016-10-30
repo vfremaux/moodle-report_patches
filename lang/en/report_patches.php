3e6119f4b97776d10164a0ef15c7b1cbb0f07821
valery <valery.fremaux@gmail.com>
2016-10-30 18:11:30 +0100

    Automated sync from SVN

--- lang/en/report_patches.php
+++ lang/en/report_patches.php
@@ -1,4 +1,25 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @author Valery Fremaux valery@valeisti.fr
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package report_patches
+ * @category report
+ */
 
 $string['config_patches_closepattern'] = 'Patch closing pattern';
 $string['config_patches_openpattern'] = 'Patch opening pattern';
