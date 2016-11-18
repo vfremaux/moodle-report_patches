0e479e6738ffc1a2c614c003df7314bb348dcc29
valery <valery.fremaux@gmail.com>
2016-11-18 16:17:45 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version detals.
+ * Version details.
  *
  * @package     report_patches
  * @category    report
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2016103000;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014050800;        // Requires this Moodle version.
 $plugin->component = 'report_patches'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '2.7.0 (Build 2012062800)';
+$plugin->release = '2.7.0 (Build 2016103000)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
