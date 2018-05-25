d46e45d31a0ccf0c3b66ee5f00fec6ec4bfa26d9
valery <valery.fremaux@gmail.com>
2018-05-25 13:22:49 +0200

    Fix PATH in install.xml

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2016103000;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016051900;        // Requires this Moodle version.
+$plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'report_patches'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.1.0 (Build 2016103000)';
+$plugin->release = '3.3.0 (Build 2016103000)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '3.3.0000';
